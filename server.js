4c287396701a54a9bcebbe06aab9ae4aab50e0b6
albatros444 <lionelandresovich@mail.ru>
2025-07-31 23:04:42 +0400

    functionality for defenitions added

--- server.js
+++ server.js
@@ -23,19 +23,29 @@ const client = new GigaChat({
 
 client.updateToken()
 app.post("/",(req, res)=>{
-    console.log("got request", req.body.wordForSentences)
+    console.log("got request", req.body)
     
-    
+    let content
+    if(req.body.type === "sentences"){
+      content = `Give me 5 simple sentences using the word ${req.body.wordForSentences}. Give no explanations`
+    }else if(req.body.type === "definition"){
+      content = `What's this ${req.body.wordForDefinition}. Answer must be in english. Answer must contain definition, key features, how it works and why it matters` 
+    }
     try{
         client
           .chat({
             messages: [{ role: 'user', 
-                         content: `Give me 5 simple sentences 
-         using the word ${req.body.wordForSentences}. Give no explanations` }],
+                         content }],
           })
           .then((resp) => {
             console.log("got response from gigachat", resp.choices[0]?.message.content)
-            res.send(resp.choices[0]?.message.content);
+            if(req.body.type === "sentences"){
+              const splSentences = resp.choices[0]?.message.content.split(/[1-5]./)
+              res.send({type:req.body.type, response: splSentences});
+            }else if(req.body.type === "definition"){
+              res.send({type:req.body.type, response: resp.choices[0]?.message.content});
+
+            }
           });
     }catch(err){
         console.log(err)
